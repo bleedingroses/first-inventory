50ccb3a5568da81b236db5814a8fda6ffc4fcceb
bleedingroses <almighty.ridho@gmail.com>
2026-03-29 10:49:36 +0000

    fix: remove factory usage in seeder

--- database/seeders/SupplierSeeder.php
+++ database/seeders/SupplierSeeder.php
@@ -13,6 +13,29 @@ class SupplierSeeder extends Seeder
      */
     public function run(): void
     {
-        Supplier::factory()->count(25)->create();
+        Supplier::create([
+            'name' => 'PT Sinar Jaya',
+            'contact_person' => '0812121334789',
+            'email' => 'ilhamsiilham@gmail.com',
+            'phone' => '0812121334789',
+            'address' => 'Alamat Ilham',
+            'notes' => 'Catatan',
+        ]);
+        Supplier::create([
+            'name' => 'UD Jiwa Muda',
+            'contact_person' => '0812121334789',
+            'email' => 'ilhamsiilham@gmail.com',
+            'phone' => '0812121334789',
+            'address' => 'Alamat Ilham',
+            'notes' => 'Catatan',
+        ]);
+        Supplier::create([
+            'name' => 'CV Sinar Gemilang',
+            'contact_person' => '0812121334789',
+            'email' => 'ilhamsiilham@gmail.com',
+            'phone' => '0812121334789',
+            'address' => 'Alamat Ilham',
+            'notes' => 'Catatan',
+        ]);
     }
 }
